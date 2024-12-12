12a4e4dd76e59fa7344a787f6d337ff5e1ae4eea
illenko <kostiaillenko@gmail.com>
2024-12-12 10:38:08 +0200

    Add microservice templates

---
+++ users/src/test/kotlin/com/example/UsersTest.kt
@@ -0,0 +1,13 @@
+package com.example
+
+import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.test.extensions.kotest5.annotation.MicronautTest
+import io.kotest.core.spec.style.StringSpec
+
+@MicronautTest
+class UsersTest(private val application: EmbeddedApplication<*>) : StringSpec({
+
+    "test the server is running" {
+        assert(application.isRunning)
+    }
+})
